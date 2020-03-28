ebed90cd976d6654d108cbc8ee20c0bb9df5be51
Maria <maria.halvorsen@hotmail.no>
2020-03-28 13:31:08 +0100

    TableOverview kode burde funke
    Mangler objektene.

--- Datamaskin/Kodelinjer/src/org/openjfx/Controller/FXMLController.java
+++ Datamaskin/Kodelinjer/src/org/openjfx/Controller/FXMLController.java
@@ -4,7 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 import org.openjfx.Model.Interfaces.scenebytte;
+import org.openjfx.Models.KomponenterTableView;
 
 public class FXMLController {
     
@@ -13,11 +17,27 @@ public class FXMLController {
 
     @FXML
     private Button newProduct;
+
+    @FXML
+    TableView <KomponenterTableView> komponenter;
+
+    @FXML
+    TableColumn<KomponenterTableView, String> produktnavn, kategori;
+
+    @FXML
+    TableColumn<KomponenterTableView, Double> pris;
     
     public void initialize() {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
 //        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        populateTable();
+    }
+
+    public void populateTable() {
+        produktnavn.setCellValueFactory(cellData -> cellData.getValue().navnProperty());
+        kategori.setCellValueFactory(cellData -> cellData.getValue().kategoriProperty());
+        pris.setCellValueFactory(cellData -> cellData.getValue().prisProperty().asObject());
     }
 
     @FXML
---
+++ Datamaskin/Kodelinjer/src/org/openjfx/Models/KomponenterTableView.java
@@ -0,0 +1,52 @@
+package org.openjfx.Models;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class KomponenterTableView {
+
+    private SimpleStringProperty navn, kategori;
+    private SimpleDoubleProperty pris;
+
+    public KomponenterTableView(String navn, String kategori, double pris) {
+        this.navn = new SimpleStringProperty(navn);
+        this.kategori = new SimpleStringProperty(kategori);
+        this.pris = new SimpleDoubleProperty(pris);
+    }
+
+    public String getNavn() {
+        return navn.get();
+    }
+
+    public SimpleStringProperty navnProperty() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn.set(navn);
+    }
+
+    public String getKategori() {
+        return kategori.get();
+    }
+
+    public SimpleStringProperty kategoriProperty() {
+        return kategori;
+    }
+
+    public void setKategori(String kategori) {
+        this.kategori.set(kategori);
+    }
+
+    public double getPris() {
+        return pris.get();
+    }
+
+    public SimpleDoubleProperty prisProperty() {
+        return pris;
+    }
+
+    public void setPris(double pris) {
+        this.pris.set(pris);
+    }
+}
