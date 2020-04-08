384b0791d19c303b496364a1286229755a61fb21
MTeresita <s326325@oslomet.no>
2020-04-08 17:09:35 +0200

    button der du kan velge å logge inn som admin eller bruker

--- Datamaskin/Kodelinjer/src/org/openjfx/Controller/LoggInnBrukerController.java
+++ Datamaskin/Kodelinjer/src/org/openjfx/Controller/LoggInnBrukerController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
 import org.openjfx.HjelpeKlasser.BrukerRegister;
 import org.openjfx.Models.Filbehandling.FilSkriving.WriteTo;
 
@@ -19,22 +20,26 @@ public class LoggInnBrukerController {
     TextField txtuser, txtadminuser, txtuser1;
 
     @FXML
-    PasswordField txtpass, txtadminpass, txtpass1;
+    PasswordField txtpass, txtadminpass, txtpass1, txtpass11;
 
     @FXML
     Label lblMessage;
 
     @FXML
-    Button btnLoginBruker, btnLoginAdmin, btnRegistrer;
+    Button btnLoginBruker, btnLoginAdmin, btnRegistrer, admin, bruker;
 
     @FXML
+    Pane brukerPane, adminPane;
 
+    public void initialize(){
+        //brukerPane.setVisible(false);
+        adminPane.setVisible(false);
+    }
+    
     public void loginEventBruker() throws Exception {
         verifyLogin(txtuser.getText(), txtpass.getText(), "./Brukere.csv",
                             "/org/openjfx/View/scene.fxml", btnLoginBruker, lblMessage);
     }
-
-
 
     public void loginEventAdmin(ActionEvent actionEvent) throws IOException {
         verifyLogin(txtadminuser.getText(), txtadminpass.getText(), "./Admin.csv",
@@ -43,23 +48,38 @@ public class LoggInnBrukerController {
     }
 
     public void registerEvent(ActionEvent actionEvent) throws IOException {
-        if (checkExistingBruker(txtuser1.getText(), "./Brukere.csv")){
-            showAlertWindow(Alert.AlertType.ERROR,windowHelper(btnRegistrer), "Bruker eksisterer",
-                    "\nBrukere eksisterer." +
-                            "\nPrøv igjen!");
-            txtuser1.clear();
-            txtpass1.clear();
+        if(txtpass1.getText().equals(txtpass11.getText())) {
+            if (checkExistingBruker(txtuser1.getText(), "./Brukere.csv")) {
+                showAlertWindow(Alert.AlertType.ERROR, windowHelper(btnRegistrer), "Bruker eksisterer",
+                        "\nBrukere eksisterer." +
+                                "\nPrøv igjen!");
+                txtuser1.clear();
+                txtpass1.clear();
+                txtpass11.clear();
+            } else {
+                BrukerRegister enBruker = new BrukerRegister(txtuser1.getText(), txtpass1.getText());
+                WriteTo.writeToCSVFile(new WriteTo(), enBruker, "./Brukere.csv");
+
+                showAlertWindow(Alert.AlertType.INFORMATION, windowHelper(btnRegistrer), "Velkommen",
+                        "Bruker opprettet");
+                reloadPage(btnRegistrer, "/org/openjfx/View/loggInnBruker.fxml");
+            }
         }
         else {
-            BrukerRegister enBruker = new BrukerRegister(txtuser1.getText(), txtpass1.getText());
-            WriteTo.writeToCSVFile(new WriteTo(), enBruker, "./Brukere.csv");
-
-            showAlertWindow(Alert.AlertType.INFORMATION,windowHelper(btnRegistrer), "Velkommen",
-                    "Bruker opprrettet");
-            System.out.println("Bruker eksisterer");
-            reloadPage(btnRegistrer, "/org/openjfx/View/loggInnBruker.fxml");
-
-
+            lblMessage.setText("Passordene er ikke like");
+            txtpass1.clear();
+            txtpass11.clear();
         }
+    }
+
+    public void adminbtn(ActionEvent actionEvent) {
+        adminPane.setVisible(true);
+        brukerPane.setVisible(false);
+    }
+
+    public void brukerbtn(ActionEvent actionEvent) {
+        brukerPane.setVisible(true);
+        adminPane.setVisible(false);
+
     }
 }
