fb9ee990b8649c036ebdb9b9649b590dd0a1c136
MTeresita <s326325@oslomet.no>
2020-04-09 13:16:58 +0200

    noen små fix

--- Datamaskin/Kodelinjer/src/org/openjfx/Models/Formaterer/KonfigurasjonsFormatererAdmin.java
+++ Datamaskin/Kodelinjer/src/org/openjfx/Models/Formaterer/KonfigurasjonsFormatererAdmin.java
@@ -1,6 +1,6 @@
 package org.openjfx.Models.Formaterer;
 
-import org.openjfx.Models.KomponenterTableView;
+import org.openjfx.Models.Komponent;
 
 import java.text.ParseException;
 import java.util.List;
@@ -9,13 +9,13 @@ public class KonfigurasjonsFormatererAdmin {
     public static String DELIMITER=";";
 
     //formaterer Komponenter-objekt med ; i mellom.
-    public static String formaterKomponenterListe(KomponenterTableView komponent){
+    public static String formaterKomponenterListe(Komponent komponent){
         return komponent.getKategori()+DELIMITER+ komponent.getNavn()+ DELIMITER+ komponent.getPris();
     }
-    public static String formaterPersoner(List<KomponenterTableView> plist) throws ParseException {
+    public static String formaterPersoner(List<Komponent> plist) throws ParseException {
         //type string klasse som kan dynamsik utvides på en god måte.
         StringBuffer str =  new StringBuffer();
-        for(KomponenterTableView p: plist){
+        for(Komponent p: plist){
             str.append(formaterKomponenterListe(p));
             str.append("\n");
         }
--- Datamaskin/Kodelinjer/src/org/openjfx/Models/Formaterer/KonfigurasjonsFormatererBruker.java
+++ Datamaskin/Kodelinjer/src/org/openjfx/Models/Formaterer/KonfigurasjonsFormatererBruker.java
@@ -1,6 +1,6 @@
 package org.openjfx.Models.Formaterer;
 
-import org.openjfx.Models.KomponenterTableView;
+import org.openjfx.Models.Komponent;
 
 import java.text.ParseException;
 import java.util.List;
@@ -10,14 +10,14 @@ public class KonfigurasjonsFormatererBruker {
     public static String DELIMITER=";";
 
     //formaterer Komponenter-objekt med ; i mellom.
-    public static String formaterKonfigurasjon(KomponenterTableView valg){
+    public static String formaterKonfigurasjon(Komponent valg){
         return valg.getNavn()+DELIMITER+ valg.getKategori()+ DELIMITER+ valg.getPris();
     }
 
-    public static String formaterPersoner(List<KomponenterTableView> plist) throws ParseException {
+    public static String formaterPersoner(List<Komponent> plist) throws ParseException {
         //type string klasse som kan dynamsik utvides på en god måte.
         StringBuffer str =  new StringBuffer();
-        for(KomponenterTableView p: plist){
+        for(Komponent p: plist){
             str.append(formaterKonfigurasjon(p));
             str.append("\n");
         }
