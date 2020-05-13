921761de68a951dc60939fe011a5ba99d8e38e98
Camilla92 <44602961+Camilla92@users.noreply.github.com>
2020-05-13 14:33:02 +0200

    fjernet klasser og metoder som ikke brukes!

--- Datamaskin/Kodelinjer/src/org/openjfx/Models/Formaterer/KonfigurasjonsFormatererAdmin.java
+++ Datamaskin/Kodelinjer/src/org/openjfx/Models/Formaterer/KonfigurasjonsFormatererAdmin.java
@@ -2,23 +2,11 @@ package org.openjfx.Models.Formaterer;
 
 import org.openjfx.Models.Komponent;
 
-import java.text.ParseException;
-import java.util.List;
-
 public class KonfigurasjonsFormatererAdmin {
     public static String DELIMITER=";";
 
     //formaterer Komponenter-objekt med ; i mellom.
     public static String formaterKomponenterListe(Komponent komponent){
         return komponent.getKategori()+DELIMITER+ komponent.getNavn()+ DELIMITER+ komponent.getPris();
-    }
-    public static String formaterPersoner(List<Komponent> plist) throws ParseException {
-        //type string klasse som kan dynamsik utvides på en god måte.
-        StringBuffer str =  new StringBuffer();
-        for(Komponent p: plist){
-            str.append(formaterKomponenterListe(p));
-            str.append("\n");
-        }
-        return str.toString();
     }
 }
--- Datamaskin/Kodelinjer/src/org/openjfx/Models/Interfaces/FilLagring.java
+++
@@ -1,9 +0,0 @@
-package org.openjfx.Models.Interfaces;
-
-import java.io.IOException;
-import java.nio.file.Path;
-
-//TODO denne blir ikke brukt, slette?
-public interface FilLagring {
-    public void skrivString(String str, Path path) throws IOException;
-}
