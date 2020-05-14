4dec301f9934c821d5a883695d3cc03334f180f9
Camilla92 <44602961+Camilla92@users.noreply.github.com>
2020-05-14 23:00:39 +0200

    kommentar

--- Datamaskin/Kodelinjer/src/org/openjfx/Models/Validering/ValiderCSVFil.java
+++ Datamaskin/Kodelinjer/src/org/openjfx/Models/Validering/ValiderCSVFil.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+//BLIR IKKE BRUKT
 public class ValiderCSVFil {
 
     private final String TXT_STRENG_KOMPONENTER = "^([A-ZÆØÅa-zæøå0-9 ]{2,50};)([A-ZÆØÅa-zæøå ]{2,50};)((?:[1-9]\\d.{2,7}|1);)([a-z]{4,5};)$";
@@ -89,11 +90,6 @@ public class ValiderCSVFil {
     }
 
 
-    public static void main(String [] args){
-        ValiderCSVFil test= new ValiderCSVFil();
-        test.testKomponenterFil("komponenter.csv");
-        test.testAdminBrukerCSVFil("Admin.csv");
-        test.testBrukerCSVFil("Brukere.csv");
-    }
+
 }
 
